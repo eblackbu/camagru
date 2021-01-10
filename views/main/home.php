8fbb172ec7ca02cfbdcdb93f58231b962017dc48
fedinikita <conifus@yandex.ru>
2021-01-10 20:58:55 +0300

    home page layout

--- views/main/home.php
+++ views/main/home.php
@@ -1,3 +1,35 @@
 <?php
-// Страница для просмотра своих фото
-echo 'user home page';
+require_once __DIR__ . '/../blocks/header.php';
+?>
+
+<div class="home">
+    <div class="home__sidebar">
+        <?php
+            require __DIR__ . '/../blocks/sidebar.php';
+        ?>
+    </div>
+    <div class="home__main">
+        <a href="#"><div class="home__main-new">
+            Cделать новую публикацию
+        </div></a>
+        <div class="home__main-posts">
+            <div class="home__main-posts-item" swo="11"><img src="/views/image/1.jpg" alt=""></div>
+            <div class="home__main-posts-item"><img src="/views/image/2.jpg" alt=""></div>
+            <div class="home__main-posts-item"><img src="/views/image/3.jpg" alt=""></div>
+            <div class="home__main-posts-item"><img src="/views/image/4.jpg" alt=""></div>
+            <div class="home__main-posts-item"><img src="/views/image/5.jpg" alt=""></div>
+        </div>
+        <div class="home__main-posts-modal">
+            <div class="home__main-posts-modal-base">
+                <div class="home__main-posts-modal-base-close">
+                    X
+                </div>
+            </div>
+            <div class="home__main-posts-modal-filler"></div>
+        </div>
+        
+    </div>
+</div>
+
+<?php
+require_once __DIR__ . '/../blocks/footer.php';
--- views/scripts/main.js
+++ views/scripts/main.js
@@ -13,3 +13,26 @@ $(document).on('click','.header__notifications', function(){
     $('.header__notifications').addClass('active');
     setTimeout(hide_notifications, 900);   
 });
+
+$(document).on('click','.header__content-menu', function(){
+    $('.header__content-sidebar').toggle();
+    if($('.header__content-menu').hasClass('active')) {
+        $('.header__content-menu').removeClass('active');
+    } else {
+        $('.header__content-menu').addClass('active');
+    }
+});
+
+$(document).on('click','.home__main-posts-item', function(){
+    // alert($(this).attr('swo'));
+    $(`<div class="home__main-posts-modal-base-content"></div>`).appendTo(".home__main-posts-modal-base");
+    $(`<div class="home__main-posts-modal-base-content-title">Title</div>`).appendTo(".home__main-posts-modal-base-content");
+    $(`<div class="home__main-posts-modal-base-content-image">${$(this).html()}</div>`).appendTo(".home__main-posts-modal-base-content");
+    $(`<div class="home__main-posts-modal-base-content-options">Options</div>`).appendTo(".home__main-posts-modal-base-content");
+    $('.home__main-posts-modal').show();
+});
+
+$(document).on('click','.home__main-posts-modal-base-close', function(){
+    $('.home__main-posts-modal').hide();
+    $(".home__main-posts-modal-base-content").remove();
+});
