9bd5921960b5c7e1bf25287db5af0a3741ac0a58
fedinikita <conifus@yandex.ru>
2021-01-12 17:26:20 +0300

    change page

--- views/blocks/sidebar.php
+++ views/blocks/sidebar.php
@@ -1,10 +1,10 @@
 <div class="sidebar">
     <div class="sidebar__profile group">
         <div class="sidebar__profile-avatar">
-            <img src="/views/image/drochila.jpg" alt="">
+            <a href="/home"><img src="/views/image/drochila.jpg" alt=""></a>
         </div>
         <div class="sidebar__profile-nickname">
-            adskiy_drochila
+            <a href="/home">adskiy_drochila</a>
         </div>
     </div>
     <div class="sidebar__statistics group">
@@ -12,8 +12,7 @@
         <div class="sidebar__statistics-subscribers"><a href="">Подписчики: 228</a></div>
     </div>
     <div class="sidebar__options group">
-        <div class="sidebar__options-rename"><a href="">Смена ника</a></div>
-        <div class="sidebar__options-repassword"><a href="">Смена пароля</a></div>
+        <div class="sidebar__options-settings"><a href="/change_password">Настройки</a></div>
         <div class="sidebar__options-logout"><a href="">Выйти</a></div>
     </div>
 </div>
